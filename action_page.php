25b6319295a57e17f69e14f48a65fa77d7d71866
Harry <hpreston2909@gmail.com>
2020-10-13 18:34:36 +0100

    added several headers to avoid spam protection

--- action_page.php
+++ action_page.php
@@ -11,6 +11,11 @@ $email_body = "Name: $name.\n". "Email: $email.\n". "Message: $message.\n";
 $to ="preston.harry99@gmail.com";
 $headers = "From: $email_form \r\n";
 $headers .= "Reply-To: $email \r\n";
+$headers .= "Organization: Sender Organization\r\n";
+$headers .= "MIME-Version: 1.0\r\n";
+$headers .= "Content-type: text/plain; charset=iso-8859-1\r\n";
+$headers .= "X-Priority: 3\r\n";
+$headers .= "X-Mailer: PHP". phpversion() ."\r\n";
 
 mail($to,$email_subject,$email_body,$headers);
 
